436e48fd822f1cdeea12554dffbb5566b265173e
Eric Constantinides <eric@ericconstantinides.com>
2017-09-29 02:15:11 -0700

    new Venue model

--- models/Venue.js
+++ models/Venue.js
@@ -3,51 +3,41 @@ const Schema = mongoose.Schema
 const enumerables = require('../enumerables')
 
 const VenueSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  region: {
-    type: Schema.Types.ObjectId,
-    ref: 'Region'
-  },
-  neighborhood: String,
-  googlePlacesId: {
-    type: String,
-    unique: true
-  },
-  yId: {
-    type: String,
-    unique: true
-  },
-  yMetaData: Object,
-  gMetaData: Object,
+  name: { type: String, required: true },
+  region: { type: Schema.Types.ObjectId, ref: 'Region' },
+  gId: { type: String, unique: true },
+  yId: { type: String, unique: true },
   zomatoId: Number,
+  updated: Date,
+  created: Date,
+  neighborhood: String,
+  yMeta: Object,
+  gMeta: Object,
   fullBar: Boolean,
-  researchNotes: String,
-  phone: String,
-  address: String,
-  position: {
-    lat: Number,
-    lng: Number
+  address: {
+    street: String,
+    city: String,
+    state: { type: String, enum: enumerables.states },
+    zip: Number
   },
-  hours: [{
-    category: {
-      type: String,
-      enum: enumerables.timeCategories
-    },
-    startTime: {
-      type: String,
-      enum: enumerables.times
-    },
-    endTime: {
-      type: String,
-      enum: enumerables.times
-    },
-    days: [{
-      type: String,
-      enum: enumerables.days
-    }]
+  phone: String,
+  lat: Number,
+  lng: Number,
+  funTimes: [{
+    category: { type: String, enum: enumerables.timeCategories },
+    startTime: { type: String, enum: enumerables.times },
+    endTime: { type: String, enum: enumerables.times },
+    days: [{ type: String, enum: enumerables.days }],
+    remarks: String
+  }],
+  funItems: [{
+    name: String,
+    price: Number
+  }],
+  research: [{
+    url: { type: String },
+    remarks: { type: String },
+    updated: { type: String }
   }],
   images: [{
     fileName: String,
@@ -55,19 +45,12 @@ const VenueSchema = new Schema({
     height: Number,
     width: Number
   }],
-  menuItems: [{
-    name: String,
-    price: Number
-  }],
   website: String,
-  links: {
-    facebook: String,
-    yelp: String,
-    openTable: String,
-    tripAdvisor: String,
-    zagat: String,
-    zomato: String
-  }
+  facebookUrl: String,
+  openTableUrl: String,
+  tripAdvisorUrl: String,
+  zagatUrl: String,
+  zomatoUrl: String
 })
 
 module.exports = mongoose.model('Venue', VenueSchema)
