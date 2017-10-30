9723d90ed1b7b432e0b948133da41ce055db2205
Eric Constantinides <eric@ericconstantinides.com>
2017-10-30 00:31:06 -0700

    temporarily disable POST, DELETE, and PUT routes

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,21 @@ const methods = require('../controllers/methods')
 
 router.route('/regions')
   .get(regions.region_list)
-  .post(regions.region_create)
+  // .post(regions.region_create)
 
 router.route('/regions/:regionId')
   .get(regions.region_detail)
-  .put(regions.region_update)
-  .delete(regions.region_delete)
+  // .put(regions.region_update)
+  // .delete(regions.region_delete)
 
 router.route('/venues')
   .get(venues.venue_list)
-  .post(venues.venue_create)
+  // .post(venues.venue_create)
 
 router.route('/venues/:venueId')
   .get(venues.venue_detail)
-  .put(venues.venue_update)
-  .delete(venues.venue_delete)
+  // .put(venues.venue_update)
+  // .delete(venues.venue_delete)
 
 router.route('/methods/yelpPhoneSearch').get(methods.yelp_phone_search)
 router.route('/methods/yelpIdSearch').get(methods.yelp_id_search)
