949eba694e3b9915d1c7d0b4d42374597ae9737c
Eric Constantinides <eric@ericconstantinides.com>
2017-11-01 22:55:11 -0700

    Move sitemap back to /api/v1/sitemap

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const morgan = require('morgan')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
-const pages = require('./controllers/pages')
 
 require('dotenv').config()
 
@@ -31,7 +30,6 @@ const routes = require('./routes')
 
 // register the route
 app.use('/api/v1', routes)
-app.get('/api/sitemap.xml', pages.sitemap)
 // NOT YET:
 // app.use('/', authRoutes)
 
